d06692413ec881ec558318a298f0fc64f6f7fb59
luis <luisguibarreto8@gmail.com>
2023-05-25 23:04:50 -0300

    Adicionando validação em javaScript

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+
+
+function teste(){
+    var nome = document.querySelector('.Nome').value;
+    var email = document.querySelector('.Email').value;
+    var mensagem = document.querySelector('.input-msg').value;
+    if (nome === "" || email === "" || mensagem === ""){
+        alert('Por favor preencha todos os campos.')
+    } else{
+        alert('Mensagem enviada!')
+    }
+}
+
+
 document.querySelector('.contato').addEventListener('click', function() {
     var elementoDestino = document.querySelector('.contatar');
     elementoDestino.scrollIntoView({ behavior: 'smooth' });
   });
 
 
-var button = document.getElementById('#darkmode');
-var div = document.querySelector('.cabecalho');
-var contactDiv = document.getElementById('contact');
-var falaDiv = document.querySelector('.fala');
-var originalColor = window.getComputedStyle(div).backgroundColor;
-var contactOriginalColor = window.getComputedStyle(contactDiv).backgroundColor;
-var falaOriginalColor = window.getComputedStyle(falaDiv).backgroundColor;
-var isWhite = false;
+  var button = document.getElementById('#darkmode');
+  var div = document.querySelector('.cabecalho');
+  var contactDiv = document.getElementById('contact');
+  var falaDiv = document.querySelector('.fala');
+  var originalColor = window.getComputedStyle(div).backgroundColor;
+  var contactOriginalColor = window.getComputedStyle(contactDiv).backgroundColor;
+  var falaOriginalColor = window.getComputedStyle(falaDiv).backgroundColor;
+  var isWhite = false;
 
 function mudarCor() {
+
   if (isWhite) {
     div.style.backgroundColor = originalColor;
     contactDiv.style.backgroundColor = contactOriginalColor;
@@ -29,6 +44,5 @@ function mudarCor() {
 
 
 button.addEventListener('click', changeColor);
-
 
 
