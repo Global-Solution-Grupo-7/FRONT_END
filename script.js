70e1dc74f841ff49a212072f2ac970c0a109f1f9
luis <luisguibarreto8@gmail.com>
2023-05-31 16:02:08 -0300

    Adicionando o footer

--- script.js
+++ script.js
@@ -18,30 +18,33 @@ document.querySelector('.contato').addEventListener('click', function() {
   });
 
 
-  var button = document.getElementById('#darkmode');
+  var button = document.getElementById('darkmode');
   var div = document.querySelector('.cabecalho');
   var contactDiv = document.getElementById('contact');
   var falaDiv = document.querySelector('.fala');
+  var footer = document.querySelector('.divFooter');
   var originalColor = window.getComputedStyle(div).backgroundColor;
   var contactOriginalColor = window.getComputedStyle(contactDiv).backgroundColor;
   var falaOriginalColor = window.getComputedStyle(falaDiv).backgroundColor;
+  var footerOriginalColor = window.getComputedStyle(footer).backgroundColor;
   var isWhite = false;
-
-function mudarCor() {
-
-  if (isWhite) {
-    div.style.backgroundColor = originalColor;
-    contactDiv.style.backgroundColor = contactOriginalColor;
-    falaDiv.style.backgroundColor = falaOriginalColor
-    isWhite = false;
-  } else {
-    div.style.backgroundColor = 'white';
-    contactDiv.style.backgroundColor = 'white';
-    falaDiv.style.backgroundColor = 'grey';
-    isWhite = true;
+  
+  function mudarCor() {
+    if (isWhite) {
+      div.style.backgroundColor = originalColor;
+      contactDiv.style.backgroundColor = contactOriginalColor;
+      falaDiv.style.backgroundColor = falaOriginalColor;
+      footer.style.backgroundColor = footerOriginalColor;
+      isWhite = false;
+    } else {
+      div.style.backgroundColor = 'white';
+      contactDiv.style.backgroundColor = 'white';
+      falaDiv.style.backgroundColor = 'grey';
+      footer.style.backgroundColor = 'grey';
+      isWhite = true;
+    }
   }
-}
-
+  
 
 button.addEventListener('click', changeColor);
 
